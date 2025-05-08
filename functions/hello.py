85df50c71ab971e69ac4de5c6bbc98bce8dcf37f
Joyline Wanjiku <joylinewanjikukingori@gmail.com>
2025-05-08 17:15:37 +0300

    new changes

---
+++ functions/hello.py
@@ -0,0 +1,15 @@
+def hello():
+    print("Hello Akirachix")
+def say_name(name):
+    print(f"hello {name}")
+def greeting_names(names):
+    for name in names:
+        print(f"Hello {name}")
+def greeting_students(**kwargs):
+    print(kwargs)
+def year_of_birth(*name):
+    year =2025 - age
+    print(f"Hello {name}, born in {year} ")
+def my_country(name= "Uganda"):
+    print(f"I love my country {name}")
+
